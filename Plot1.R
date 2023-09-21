8c50052ece53d158679fc1d454f50a294320dd5f
battleblake7 <84210032+battleblake7@users.noreply.github.com>
2023-09-21 09:03:42 -0500

    Update Plot1.R

--- Plot1.R
+++ Plot1.R
@@ -9,38 +9,3 @@ dev.off()
 
 
 
-Sys.setlocale("LC_TIME", "English")
-
-date_converted <- as.Date(data[, 1], format = "%d/%m/%Y")
-time_converted <- strptime(data[, 2], format = "%H:%M:%S")
-time_axis <- as.POSIXct(paste(date_converted, data[, 2]))
-
-plot(time_axis, data[, 3], xlab = "", ylab = "Global Active Power (kilowatts)", type = "l")
-dev.copy(png, filename="plot2.png")
-dev.off()
-
-plot(time_axis, data[, 7], xlab = "", ylab = "Energy sub metering", type = "l")
-lines(time_axis, data[, 8], col = "red")
-lines(time_axis, data[, 9], col = "blue")
-
-legend("topright", c("Sub_metering_1", "Sub_metering_2", "Sub_metering_3"), 
-       col = c("black","red","blue"), lty = 1)
-dev.copy(png, filename="plot3.png")
-dev.off()
-
-par(mfrow = c(2,2))
-
-plot(time_axis, data[, 3], xlab = "", ylab = "Global Active Power (kilowatts)", type = "l")
-
-plot(time_axis, data[, 5], xlab = "datetime", ylab = "Voltage", type = "l")
-
-plot(time_axis, data[, 7], xlab = "", ylab = "Energy sub metering", type = "l")
-lines(time_axis, data[, 8], col = "red")
-lines(time_axis, data[, 9], col = "blue")
-
-legend("topright", c("Sub_metering_1", "Sub_metering_2", "Sub_metering_3"), 
-       col = c("black","red","blue"), cex = 0.8, lty = 1 , bty = "n")
-
-plot(time_axis, data[, 4], xlab = "datetime", ylab = "Global_reactive_power", type = "l")
-dev.copy(png, filename="plot4.png")
-dev.off()
