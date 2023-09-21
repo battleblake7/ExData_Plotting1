7cad4d3fe1164ef0cf193883c4857fe84b608319
battleblake7 <84210032+battleblake7@users.noreply.github.com>
2023-09-21 08:45:44 -0500

    Add files via upload

---
+++ Plot4.R
@@ -0,0 +1,11 @@
+
+
+Sys.setlocale("LC_TIME", "English")
+
+date_converted <- as.Date(data[, 1], format = "%d/%m/%Y")
+time_converted <- strptime(data[, 2], format = "%H:%M:%S")
+time_axis <- as.POSIXct(paste(date_converted, data[, 2]))
+
+plot(time_axis, data[, 3], xlab = "", ylab = "Global Active Power (kilowatts)", type = "l")
+dev.copy(png, filename="plot2.png")
+dev.off()
